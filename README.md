e413469c184a9703d75e4efa3037e35d2726cfaa
Kieren Evans <kevans@inviqa.com>
2016-06-06 18:13:10 +0100

    Initial commit from `chef generate cookbook iptables-patterns` using chefdk 0.14.25

---
+++ README.md
@@ -0,0 +1,4 @@
+# iptables-patterns
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'iptables-patterns'
+maintainer 'Inviqa UK LTD'
+maintainer_email 'kevans@inviqa.com'
+license 'apachev2'
+description 'Installs/Configures iptables-patterns'
+long_description 'Installs/Configures iptables-patterns'
+version '0.1.0'
