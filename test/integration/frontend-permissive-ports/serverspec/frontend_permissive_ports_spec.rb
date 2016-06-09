e761c1061bff7f80c925dde51b060325318cbaa5
Kieren Evans <kevans@inviqa.com>
2016-06-09 15:05:50 +0100

    Match the short form used in iptables -S in the serverspecs.

--- test/integration/frontend-permissive-ports/serverspec/frontend_permissive_ports_spec.rb
+++ test/integration/frontend-permissive-ports/serverspec/frontend_permissive_ports_spec.rb
@@ -2,62 +2,38 @@ require 'spec_helper'
 
 describe 'iptables-patterns::frontend_permissive_ports' do
   it 'uses the standard firewall rules on input' do
-    describe iptables do
-      it { should have_rule('--jump STANDARD-FIREWALL').with_chain('INPUT') }
-    end
-  end
-
-  it 'uses the standard firewall rules on forward' do
-    describe iptables do
-      it { should have_rule('--jump STANDARD-FIREWALL').with_chain('FORWARD') }
-    end
+    expect(iptables).to have_rule('-j STANDARD-FIREWALL').with_chain('INPUT')
   end
 
   it 'allows all local interface traffic' do
-    describe iptables do
-      it { should have_rule('--in-interface lo --jump ACCEPT').with_chain('STANDARD-FIREWALL') }
-    end
+    expect(iptables).to have_rule('-i lo -j ACCEPT').with_chain('STANDARD-FIREWALL')
   end
 
   it 'allows all ICMP traffic' do
-    describe iptables do
-      it { should have_rule('--protocol icmp --jump ACCEPT').with_chain('STANDARD-FIREWALL') }
-    end
+    expect(iptables).to have_rule('-p icmp -j ACCEPT').with_chain('STANDARD-FIREWALL')
   end
 
   it 'allows port 22 to be communicated with' do
-    describe iptables do
-      it { should have_rule('--protocol tcp --dport 22 --jump ACCEPT').with_chain('STANDARD-FIREWALL') }
-    end
+    expect(iptables).to have_rule('-p tcp -m tcp --dport 22 -j ACCEPT').with_chain('STANDARD-FIREWALL')
   end
 
   it 'allows port 80 to be communicated with' do
-    describe iptables do
-      it { should have_rule('--protocol tcp --dport 80 --jump ACCEPT').with_chain('STANDARD-FIREWALL') }
-    end
+    expect(iptables).to have_rule('-p tcp -m tcp --dport 80 -j ACCEPT').with_chain('STANDARD-FIREWALL')
   end
 
   it 'allows port 443 to be communicated with' do
-    describe iptables do
-      it { should have_rule('--protocol tcp --dport 443 --jump ACCEPT').with_chain('STANDARD-FIREWALL') }
-    end
+    expect(iptables).to have_rule('-p tcp -m tcp --dport 443 -j ACCEPT').with_chain('STANDARD-FIREWALL')
   end
 
   it 'allows all established traffic to continue communicating with each other' do
-    describe iptables do
-      it { should have_rule('--match state --state RELATED,ESTABLISHED --jump ACCEPT').with_chain('STANDARD-FIREWALL') }
-    end
+    expect(iptables).to have_rule('-m state --state RELATED,ESTABLISHED -j ACCEPT').with_chain('STANDARD-FIREWALL')
   end
 
   it 'rejects all other ipv4 traffic' do
-    describe iptables do
-      it { should have_rule('--jump REJECT --reject-with icmp-port-unreachable').with_chain('STANDARD-FIREWALL') }
-    end
+    expect(iptables).to have_rule('-j REJECT --reject-with icmp-port-unreachable').with_chain('STANDARD-FIREWALL')
   end
 
   it 'rejects all other ipv6 traffic' do
-    describe ip6tables do
-      it { should have_rule('--jump REJECT --reject-with icmp6-port-unreachable').with_chain('STANDARD-FIREWALL') }
-    end
+    expect(ip6tables).to have_rule('-j REJECT --reject-with icmp6-port-unreachable').with_chain('STANDARD-FIREWALL')
   end
 end
