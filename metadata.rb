1dfc7e665e2072f939cf322d8370d40fb1475e15
Kieren Evans <kevans@inviqa.com>
2016-06-13 16:44:11 +0100

    Add issues_url and source_url to metadata (fixing Foodcritic issues).

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,7 @@ description 'Installs/Configures iptables-patterns'
 long_description 'Installs/Configures iptables-patterns'
 version '0.1.0'
 
+issues_url 'https://github.com/inviqa/chef-iptables-patterns/issues'
+source_url 'https://github.com/inviqa/chef-iptables-patterns/'
+
 depends 'iptables-ng', '>= 2.2'
