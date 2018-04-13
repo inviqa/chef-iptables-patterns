3381f70888898dd4f8460b745e592fcd7ffc4cb6
Andy Thompson <athompson@inviqa.com>
2018-04-13 11:18:02 +0100

    Bump version for bugfix release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.1.1 (13 April 2018)
+
+BUG FIXES:
+
+  * #1 Fix issue in icmp ip6tables rule
+
 ## 0.1.0 (6 June 2016)
 
 FEATURES
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kevans@inviqa.com'
 license 'apachev2'
 description 'Installs/Configures iptables-patterns'
 long_description 'Installs/Configures iptables-patterns'
-version '0.1.0'
+version '0.1.1'
 
 issues_url 'https://github.com/inviqa/chef-iptables-patterns/issues'
 source_url 'https://github.com/inviqa/chef-iptables-patterns/'
